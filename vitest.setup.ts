7e16b13e6ed5278481a6586ca3dedba18f4db258
kiaravzm <kiaramartins.design@gmail.com>
2026-07-28 14:20:32 -0300

    chore: add cleanup and matchMedia mock to test setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,21 @@
 import "@testing-library/jest-dom/vitest";
+import "@testing-library/jest-dom/vitest";
+import { afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+afterEach(() => {
+  cleanup();
+});
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {}, // deprecated, mas algumas libs ainda usam
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
